46af8a68d1177db8e897507357d86c9489886ee8
Maxwell Jay <MaxwellJay@1fsc7p.onmicrosoft.com>
2022-11-14 16:11:15 +0800

    update

--- OpenCV/convert.cpp
+++ OpenCV/convert.cpp
@@ -1,19 +1,17 @@
 #include <iostream>
 #include <vector>
-#include <opencv2/opencv.hpp>
+#include <opencv.hpp>
 
-int main()
+int main(int argc, char** argv)
 {
-	cv::Mat img = cv::imread("../imgs/lena.jpg"); // 从文件读取图片到内存
-	if(img.empty()) // 判断图片是否读取成功
-	{
-		std::cerr << "读取图片失败！" << std::endl;
+	cv::Mat img = cv::imread(argv[1],-1);
+	if ( img.empty() ) {
 		return -1;
+	} else {
+		cv::namedWindow("Example1", cv::WINDOW_AUTOSIZE);
+		cv::imshow("Example1", img);
+		cv::waitKey(0);
+		cv::destroyWindow("Example1");
+		return 0;
 	}
-
-	cv::Mat converted; // 用于存储转换后的图片
-	cv::cvtColor(img, converted, cv::COLOR_BGR2GRAY); // 转换为灰度图
-
-	cv::imshow("converted", converted); // 显示转换后的图片
-	cv::waitKey(0); // 等待按键
 }
